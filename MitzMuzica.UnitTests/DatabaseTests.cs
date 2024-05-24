458f2f47c5ea00f21b7444f9e410bdbb351a3fa9
Eduard975 <theblackasasin@yahoo.com>
2024-05-24 15:14:14 +0300

    add UnitTest for getSong

---
+++ MitzMuzica.UnitTests/DatabaseTests.cs
@@ -0,0 +1,28 @@
+using MitzMuzica.DatabaseAPI;
+
+namespace MitzMuzica.UnitTests;
+
+public class DatabaseTests
+{
+    IDatabase db = new Database();
+
+    [SetUp]
+    public void Setup()
+    {
+        string path = "E:\\AC\\IP\\mitzmuzica\\MitzMuzica\\Resources\\playlistsDB.db";
+        db.EstablishConnection(path);
+    }
+
+    [Test]
+    public void TestGetSong()
+    {
+        int songId = 1;
+        string title, path;
+
+        (songId, title, path) = db.GetSong(songId);
+        
+        Assert.IsTrue(songId == 1 
+                      && title == "Cantecul bobi dog"
+                      && path == "penis", $"Valorile gasite sunt: {songId}, {title}, {path}");
+    }
+}
