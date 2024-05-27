a1a9f093fa4248cf98654e7242dccea9fdb49c4c
Eduard975 <theblackasasin@yahoo.com>
2024-05-27 15:27:15 +0300

    Fix variable and parameter name

--- MitzMuzica.DatabaseAPI/IDataBase.cs
+++ MitzMuzica.DatabaseAPI/IDataBase.cs
@@ -6,19 +6,30 @@ namespace MitzMuzica.DatabaseAPI;
 public interface IDatabase
 {
     private static SQLiteConnection? _connection = null;
-
+    /// <summary>
+    /// Creates the database if it doesn't exist already, does nothing otherwise
+    /// </summary>
     public void CreateDatabase();
-    
+    /// <summary>
+    /// Establishes a connection to the database
+    /// </summary>
+    /// <param name="path"></param>
     public void EstablishConnection(string path) { }
     
-    public void InsertNewSong(string title, string path);
+    /// <summary>
+    /// Inserts a new song in the database and returns a songId,
+    /// duplicate or null title/path will throw an sql exception
+    /// </summary>
+    /// <param name="title"></param>
+    /// <param name="path"></param>
+    public int InsertNewSong(string title, string path);
     
-    public string GetSongPath(int s_id);
+    public string GetSongPath(int songId);
     public string GetSongPath(string title);
 
     public int GetSongID(string title);
     
-    public void DeleteSong(int s_id);
+    public void DeleteSong(int songId);
     public void DeleteSong(string title);
 
     public int InsertNewPlaylist(string name, List<int> songIds);
