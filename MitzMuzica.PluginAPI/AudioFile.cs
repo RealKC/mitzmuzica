641857bf6825610ff97227a5baf74c890c42a64b
Mițca Dumitru <dumitru0mitca@gmail.com>
2024-05-27 14:29:23 +0300

    Document plugin interfaces and classes

--- MitzMuzica.PluginAPI/AudioFile.cs
+++ MitzMuzica.PluginAPI/AudioFile.cs
@@ -1,12 +1,38 @@
 namespace MitzMuzica.PluginAPI;
 
+/// <summary>
+/// An audio file as returned by a plugin
+/// </summary>
 public interface IAudioFile
 {
+    /// <summary>
+    /// Best-effort song title from file metadata
+    /// </summary>
     public string Title { get; }
+    
+    /// <summary>
+    /// Best-effort song author/artist from file metadata
+    /// </summary>
     public string Author { get; }
+    
+    /// <summary>
+    /// Best-effort song length in seconds from file metadata
+    /// </summary>
     public long Length { get; }
 
+    /// <summary>
+    /// Starts the playback of this audio file
+    /// </summary>
     public void Start();
+    
+    /// <summary>
+    /// Stops this file from playing 
+    /// </summary>
     public void Stop();
+    
+    /// <summary>
+    /// Seeks to a given offset in the audio file
+    /// </summary>
+    /// <param name="s">the offset, in seconds</param>
     public void SeekTo(long s);
 }
--- MitzMuzica.PluginLoader/PluginLoader.cs
+++ MitzMuzica.PluginLoader/PluginLoader.cs
@@ -3,17 +3,33 @@ using MitzMuzica.PluginAPI;
 
 namespace MitzMuzica.PluginLoader;
 
+/// <summary>
+/// The main entry point for loading plugins
+/// </summary>
 public class PluginLoader
 {
+    /// <summary>
+    /// Creates a PluginLoader
+    /// </summary>
+    /// <param name="pluginDirectoryPath">The path where plugins are stored</param>
     public PluginLoader(string pluginDirectoryPath)
     {
         DirectoryPath = pluginDirectoryPath;
     }
 
+    /// <summary>
+    /// A list of all audio plugins
+    /// </summary>
     public List<IAudioPlugin> AudioPlugins { get; private set; } = new();
 
+    /// <summary>
+    /// The path where plugins are stored
+    /// </summary>
     public string DirectoryPath { get; }
 
+    /// <summary>
+    /// Method to load all plugins from <see cref="DirectoryPath"/>
+    /// </summary>
     public void LoadPlugins()
     {
         var plugins = from file in Directory.EnumerateFiles(DirectoryPath, "*.dll", SearchOption.AllDirectories)
